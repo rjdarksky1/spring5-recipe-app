7f402bdf6e1c5df4f78f6efd8e1948b0cb7bfb43
rjdarkknight1 <hrjhonor@gmail.com>
2019-07-17 22:40:57 -0400

    Refactor to display recipes on the initial index page. Add recipe service.

--- src/main/java/com/example/controllers/IndexController.java
+++ src/main/java/com/example/controllers/IndexController.java
@@ -1,36 +1,23 @@
 package com.example.controllers;
 
-import com.example.domain.Category;
-import com.example.domain.UnitOfMeasure;
-import com.example.repositories.CategoryRepository;
-import com.example.repositories.UnitOfMeasureRepository;
+import com.example.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository,
-                           UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
-    public String getIndexpage() {
-        Optional<Category> categoryOptional =
-                categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional =
-                unitOfMeasureRepository.findByDescription("Teaspoon");
-
-        System.out.println("Category id: " + categoryOptional.get().getId());
-        System.out.println("UOM id:" + unitOfMeasureOptional.get().getId());
-
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(Model model) {
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 
